5540752ddd1b877a7662b858eec1765b6ef9ef5f
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2024-09-16 12:47:23 +0530

    Add logic for sunday salary calculation

--- mygstcafe/salary_calculation.py
+++ mygstcafe/salary_calculation.py
@@ -9,77 +9,115 @@ def calculate_monthly_salary(employee_data, total_working_days):
         per_day_salary = round(basic_salary / total_working_days, 2)
         
         # Initialize counters
-        total_salary = 0
-        total_late_deductions = 0
+        total_salary = 0.0
+        total_late_deductions = 0.0
         full_days = 0
         half_days = 0
         quarter_days = 0
         three_four_quarter_days = 0
         total_absents = 0
         lates = 0
-        overtime_salry = 0
+        sundays = 0
+        sundays_salry = 0.0
+        overtime_salry = 0.0
         actual_working_days = 0
         
         for record in attendance_records:
-            attendance_date = record["attendance_date"]
-            in_time = record["in_time"]
-            out_time = record["out_time"]
-            
-            ideal_check_in_time = datetime.combine(attendance_date,time(10,0))
-            ideal_check_out_time = datetime.combine(attendance_date,time(19,0))
-            overtime_threshold = datetime.combine(attendance_date,time(19,30))
-            
-            if in_time is not None and out_time is not None:
-                temp_in = in_time.time()
-                check_in = datetime.combine(attendance_date,temp_in)
-                temp_out = out_time.time()
-                check_out = datetime.combine(attendance_date,temp_out)
-                total_working_time = check_out - check_in
-                total_working_hours = total_working_time.total_seconds() / 3600 
+            if attendance_records:
+                attendance_date = record["attendance_date"]
+                in_time = record["in_time"]
+                out_time = record["out_time"]
                 
-                if 7.875 <= total_working_hours <= 10.125:
-                    overtime_salry = 0
-                    if total_working_hours > 9 and check_out > overtime_threshold:
-                        extra_time = check_out - ideal_check_out_time
-                        overtime = extra_time.total_seconds() / 60
-                        min_overtime_salary = per_day_salary / 540
-                        overtime_salry = overtime * min_overtime_salary    
-                    full_days += 1
-                    actual_working_days += 1
-                    total_salary += per_day_salary + overtime_salry
+                ideal_check_in_time = datetime.combine(attendance_date,time(10,0))
+                ideal_check_out_time = datetime.combine(attendance_date,time(19,0))
+                overtime_threshold = datetime.combine(attendance_date,time(19,30))
                 
-                elif 5.625 <= total_working_hours < 7.875:
-                    quarter_days += 1
-                    actual_working_days += 1
-                    total_salary += 0.75 * per_day_salary
-                
-                elif 3.375 <= total_working_hours < 5.625:
-                    half_days += 1
-                    actual_working_days += 1
-                    total_salary += 0.5 * per_day_salary
-                
-                elif 1.125 <= total_working_hours < 3.375:
-                    three_four_quarter_days += 1
-                    actual_working_days += 1
-                    total_salary += 0.25 * per_day_salary
-                
+                if in_time is not None and out_time is not None:
+                    temp_in = in_time.time()
+                    check_in = datetime.combine(attendance_date,temp_in)
+                    check_in_date = check_in.date()
+                    temp_out = out_time.time()
+                    check_out = datetime.combine(attendance_date,temp_out)
+                    total_working_time = check_out - check_in
+                    total_working_hours = total_working_time.total_seconds() / 3600 
+                    
+                    if 7.875 <= total_working_hours <= 10.125:
+                        overtime_salry = 0
+                        if total_working_hours > 9 and check_out > overtime_threshold:
+                            extra_time = check_out - ideal_check_out_time
+                            overtime = extra_time.total_seconds() / 60
+                            min_overtime_salary = per_day_salary / 540
+                            overtime_salry = overtime * min_overtime_salary
+                            
+                        if check_in_date.weekday() == 6:
+                            sundays_salry += per_day_salary
+                            sundays +=1
+                            actual_working_days += 1
+                        else:  
+                            full_days += 1
+                            actual_working_days += 1
+                            total_salary += per_day_salary
+                    
+                    elif 5.625 <= total_working_hours < 7.875:
+                        if check_in_date.weekday() == 6:
+                            sundays_salry += 0.75 * per_day_salary
+                            sundays +=1
+                            actual_working_days += 1
+                        else: 
+                            quarter_days += 1
+                            actual_working_days += 1
+                            total_salary += 0.75 * per_day_salary
+                    
+                    elif 3.375 <= total_working_hours < 5.625:
+                        if check_in_date.weekday() == 6:
+                            sundays_salry += 0.5 * per_day_salary
+                            sundays +=1
+                            actual_working_days += 1
+                        else:
+                            half_days += 1
+                            actual_working_days += 1
+                            total_salary += 0.5 * per_day_salary
+                        
+                    elif 1.125 <= total_working_hours < 3.375:
+                        if check_in_date.weekday() == 6:
+                            sundays_salry += 0.25 * per_day_salary
+                            sundays +=1
+                            actual_working_days += 1
+                        else:
+                            three_four_quarter_days += 1
+                            actual_working_days += 1
+                            total_salary += 0.25 * per_day_salary
+                    
+                    else:
+                        overtime_salry = 0
+                        if total_working_hours > 10.125 and check_out > overtime_threshold:
+                            if check_in_date.weekday() == 6:
+                                extra_time = check_out - ideal_check_out_time
+                                overtime = extra_time.total_seconds() / 60
+                                min_overtime_salary = per_day_salary / 540
+                                sundays_salry = overtime * min_overtime_salary
+                                sundays +=1
+                                actual_working_days += 1
+                            else:
+                                extra_time = check_out - ideal_check_out_time
+                                overtime = extra_time.total_seconds() / 60
+                                min_overtime_salary = per_day_salary / 540
+                                overtime_salry = overtime * min_overtime_salary 
+                    
+                    if check_in > ideal_check_in_time:
+                        if check_in_date.weekday() != 6:
+                            lates += 1
+                            late_deduction = 0.10 * per_day_salary
+                            total_late_deductions += late_deduction
                 else:
-                    overtime_salry = 0
-                    if total_working_hours > 10.125 and check_out > overtime_threshold:
-                        extra_time = check_out - ideal_check_out_time
-                        overtime = extra_time.total_seconds() / 60
-                        min_overtime_salary = per_day_salary / 540
-                        overtime_salry = overtime * min_overtime_salary 
-                
-                if check_in > ideal_check_in_time:
-                    lates += 1
-                    late_deduction = 0.10 * per_day_salary
-                    total_late_deductions += late_deduction
+                    total_absents += 1
+                    total_salary += 0          
             else:
                 total_absents += 1
                 total_salary += 0
         
         total_salary -= total_late_deductions
+        total_salary += sundays_salry + overtime_salry
         
         data["salary_information"] = {
             "basic_salary": basic_salary,
@@ -90,6 +128,8 @@ def calculate_monthly_salary(employee_data, total_working_days):
             "half_days": half_days,
             "quarter_days": quarter_days,
             "three_four_quarter_days": three_four_quarter_days,
+            "sundays_working_days":sundays,
+            "sundays_salry":sundays_salry,
             "total_salary": round(total_salary,2),
             "total_late_deductions": total_late_deductions,
             "absent": total_absents,
