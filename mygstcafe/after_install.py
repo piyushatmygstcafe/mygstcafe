482adef7e1abae6fd079b578abb77f5bf133054e
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2024-09-26 12:50:59 +0530

    Fix for print issue

--- mygstcafe/after_install.py
+++ mygstcafe/after_install.py
@@ -1,4 +1,4 @@
 import frappe
 
 def after_install():
-    frappe.db.set_value('DocType', 'Pay Slips', 'default_print_format', 'pay_slip')
+    frappe.db.set_value('DocType', 'Pay Slips', 'default_print_format', 'Pay Slip Format')
